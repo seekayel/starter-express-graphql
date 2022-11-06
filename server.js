93a08a4f7baa773a22b559ddbdc084545b77bd79
Kam Lasater <ckl@seekayel.com>
2022-11-05 21:44:30 -0400

    it works... whahah?

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql')
+const crypto = require('crypto')
 const CyclicDb = require('cyclic-dynamodb')
 
 const db = CyclicDb("wild-pike-clothesCyclicDB")
@@ -22,35 +23,35 @@ var app = express();
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    getCustomer(email: String!): Customer
+    getCustomerById(id: ID!): Customer
   }
   type Mutation {
     createCustomer(name: String!, email: String!): Customer
   }
   type Customer {
+    id: ID!
     name: String!,
     email: String!
   }
 `);
 
-var fakeDatabase = {};
-
-class Customer {
-  constructor(name, email) {
-    this.name = name
-    this.email = email
-  }
+function generateId() {
+  return crypto.randomBytes(10).toString('hex')
 }
 
+const col = "customers"
 // The root provides a resolver function for each API endpoint
 var root = {
-  getCustomer: ({email}) => {
-    return fakeDatabase.email
+  getCustomerById: async ({id}) => {
+    const cust =  await db.collection(col).get(id)
+    console.log(JSON.stringify(cust,null,2))
+    return cust.props
   },
-  createCustomer: ({name,email}) => {
-    let cust = new Customer(name, email);
-    fakeDatabase.email = cust
-    return cust
+  createCustomer: async ({name,email}) => {
+    const cust = {id: generateId(), name, email}
+    const res = await db.collection(col).set(cust.id, cust)
+    console.log(JSON.stringify(res,null,2))
+    return res.props
   }
 };
 
